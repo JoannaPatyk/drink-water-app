deeb9de294c0f370912c0fe0f2a787e2e1507e29
Joanna Patyk <patykjoanna93@gmail.com>
2023-04-02 15:55:00 +0200

    Added Prettier

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Drink Water App
 
 Drink Water App is a simple web application that helps track the amount of water consumed in a day. The application was built using HTML, CSS, and JavaScript technologies.
+
 ## Quick setup and start
 
 Firstly compile with `npm run compile`. Then, in order to run webpage you need HTTP sever which serve the site, for example [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer).
@@ -8,11 +9,12 @@ Firstly compile with `npm run compile`. Then, in order to run webpage you need H
 ## How to use the app
 
 Once the website is open, enter the amount of water you drink into the picture of a small cup of water. Each cup is 250 ml. To delete an entry, simply click on the mug again. The large cup displays the total amount of water you have drunk.
+
 ## App features
 
-* Adding entries of consumed water
-* Removing entries of consumed water
-* Displaying the total amount of water consumed during the day
+-   Adding entries of consumed water
+-   Removing entries of consumed water
+-   Displaying the total amount of water consumed during the day
 
 ## Technologies
 
--- src/script.ts
+++ src/script.ts
@@ -1,87 +1,87 @@
 function create_cup(): void {
-  let container: HTMLElement = document.querySelector(".cups");
+    let container: HTMLElement = document.querySelector('.cups');
 
-  for (let i = 0; i < 8; i++) {
-  const newElement = document.createElement("div");
-  newElement.classList.add("cup", "cup-small");
+    for (let i = 0; i < 8; i++) {
+        const newElement = document.createElement('div');
+        newElement.classList.add('cup', 'cup-small');
 
-  const newParagraph  = document.createElement("p");
-  newParagraph.textContent = "250 ml";
+        const newParagraph = document.createElement('p');
+        newParagraph.textContent = '250 ml';
 
-  const newImage = document.createElement("img");
-    newImage.setAttribute("src", "img/waterdrop.png");
-    newImage.setAttribute("alt", "");
+        const newImage = document.createElement('img');
+        newImage.setAttribute('src', 'img/waterdrop.png');
+        newImage.setAttribute('alt', '');
 
-    newElement.appendChild(newParagraph);
-    newElement.appendChild(newImage);
+        newElement.appendChild(newParagraph);
+        newElement.appendChild(newImage);
 
-    container.appendChild(newElement);
-  }
+        container.appendChild(newElement);
+    }
 }
 
-  create_cup();
+create_cup();
 
-const small_cups: NodeListOf<Element> = document.querySelectorAll(".cup-small");
+const small_cups: NodeListOf<Element> = document.querySelectorAll('.cup-small');
 console.log(small_cups);
-const listers: HTMLDivElement = document.querySelector("#listers");
-const percentage: HTMLDivElement = document.querySelector(".percentage");
-const remained: HTMLDivElement = document.querySelector(".remained");
-const drops: NodeListOf<HTMLImageElement> = document.querySelectorAll("img");
-const animationText: HTMLHeadingElement = document.querySelector("h2");
+const listers: HTMLDivElement = document.querySelector('#listers');
+const percentage: HTMLDivElement = document.querySelector('.percentage');
+const remained: HTMLDivElement = document.querySelector('.remained');
+const drops: NodeListOf<HTMLImageElement> = document.querySelectorAll('img');
+const animationText: HTMLHeadingElement = document.querySelector('h2');
 
 small_cups.forEach((cup: Element, index: number) => {
-  cup.addEventListener("click", () => {
-    highlight_cups(index)
-  });
+    cup.addEventListener('click', () => {
+        highlight_cups(index);
+    });
 });
 
 function highlight_cups(index: number): void {
-  if (small_cups[index].classList.contains("full") && !small_cups[index].nextElementSibling.classList.contains("full")) {
-    index--;
-  }
-
-  small_cups.forEach((cup: Element, index2: number) => {
-    if (index2 <= index) {
-      cup.classList.add("full");
-    } else {
-      cup.classList.remove("full");
+    if (
+        small_cups[index].classList.contains('full') &&
+        !small_cups[index].nextElementSibling.classList.contains('full')
+    ) {
+        index--;
     }
-  });
 
-  drops.forEach((drop: HTMLImageElement, index2: number) => {
-    if (index2 <= index) {
-      drop.style.display = "none";
-    } else {
-      drop.style.display = "block";
-    }
-  });
+    small_cups.forEach((cup: Element, index2: number) => {
+        if (index2 <= index) {
+            cup.classList.add('full');
+        } else {
+            cup.classList.remove('full');
+        }
+    });
 
-  update_big_cup();
+    drops.forEach((drop: HTMLImageElement, index2: number) => {
+        if (index2 <= index) {
+            drop.style.display = 'none';
+        } else {
+            drop.style.display = 'block';
+        }
+    });
+
+    update_big_cup();
 }
 
 function update_big_cup(): void {
-  const fullCups = document.querySelectorAll(".cup-small.full").length;
-  const totalCups = small_cups.length;
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    const totalCups = small_cups.length;
 
-  if (fullCups === 0) {
-    percentage.style.visibility = "hidden";
-    percentage.style.height = '0';
-  } else {
-    percentage.style.visibility = "visible";
-    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
-    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
-  }
+    if (fullCups === 0) {
+        percentage.style.visibility = 'hidden';
+        percentage.style.height = '0';
+    } else {
+        percentage.style.visibility = 'visible';
+        percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+        percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+    }
 
-  if (fullCups === totalCups) {
-    remained.style.visibility = "hidden";
-    remained.style.height = '0';
-    animationText.classList.add("end");
-  } else {
-    remained.style.visibility = "visible";
-    listers.innerText = `${2 - (250 * fullCups) / 1000}L`;
-    animationText.classList.remove("end");
-  }
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden';
+        remained.style.height = '0';
+        animationText.classList.add('end');
+    } else {
+        remained.style.visibility = 'visible';
+        listers.innerText = `${2 - (250 * fullCups) / 1000}L`;
+        animationText.classList.remove('end');
+    }
 }
-
-
-
